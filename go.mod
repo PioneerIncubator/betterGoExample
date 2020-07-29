007a25fbc7d0739f16ab8dab8b89fe0fd427680a
YongHaoHu <christopherwuy@gmail.com>
2020-07-29 16:50:25 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PioneerIncubator/betterGoExample
+
+go 1.14
+
+require github.com/PioneerIncubator/betterGo v0.0.0-20200728103753-4b2f6a5b7069
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PioneerIncubator/betterGo/enum"
+)
+
+func mul(a, b int) (c int) {
+	c = a * b
+	return
+}
+
+func testINT() {
+	a, b := make([]int, 10), 12
+	for i := range a {
+		a[i] = i + 1
+	}
+	// Compute 10!
+	out := enum.Reduce(a, mul, 1).(int)
+	expect := 1
+	for i := range a {
+		expect *= a[i]
+	}
+	if expect != out {
+		fmt.Printf("expected %d got %d , b %d", expect, out, b)
+	}
+	fmt.Println("success, ", expect)
+}
+
+func main() {
+	testINT()
+
+	c, d := make([]float32, 10), 12.3
+	for i := range c {
+		c[i] = float32(i) + 1.1
+	}
+	// Compute 10!
+	floatOut := enum.Reduce(c, func(x, y float32) (z float32) {
+		z = x * y
+		return
+	}, 1.0).(float32)
+	var floatExpect float32 = 1.0
+	for i := range c {
+		floatExpect *= c[i]
+	}
+	if floatExpect != floatOut {
+		fmt.Printf("expected %f got %f , b %f", floatExpect, floatOut, d)
+	}
+	fmt.Println("success, ", floatExpect)
+}
